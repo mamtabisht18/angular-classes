dd4aab5312a6e22fb2ad22face01e8f2eb6eddd2
“Mamta” <mamta.bisht@outlook.com>
2020-07-05 12:32:05 +0530

    sidebar module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,12 +3,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 const modules = [
     MatIconModule,
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
+    MatSidenavModule,
 ]
 @NgModule({
   imports: [
